4a6d1c466a787bb84a56d9212f7dbf2543dda748
Maycon J Pereira <mayconjovan@gmail.com>
2018-11-28 09:36:10 -0200

    Adicao da pagina de formulario desenhavel

---
+++ web/newJs/scripts.js
@@ -0,0 +1,7 @@
+function filterClicked(){
+   alert('O botao do filtro foi clicado') 
+
+   var advancedFilter = document.getElementById("advancedFilter");
+
+   advancedFilter.innerHTML = '<h3> eu sou o componente de filtro avançado</h3>';
+}
