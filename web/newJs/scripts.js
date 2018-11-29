55d2b3f17c69c5e3566eb4860756e98d93ffb8f5
Maycon J Pereira <mayconjovan@gmail.com>
2018-11-29 01:57:09 -0200

    Ajuste de formatação e identação de código

--- web/newJs/scripts.js
+++ web/newJs/scripts.js
@@ -1,180 +1,169 @@
-function filterClicked(){
-   alert('O botao do filtro foi clicado') 
+function filterClicked() {
+   alert('O botao do filtro foi clicado')
 
    var advancedFilter = document.getElementById("advancedFilter");
 
    advancedFilter.innerHTML = '<h3> eu sou o componente de filtro avançado</h3>';
 }
 
-
 var id = 0;
-        var opcao = 1;
-        var radio = 1;
+var opcao = 1;
+var radio = 1;
 
-        function selectText(id) {
-            document.getElementById("tituloform").select();
-        }
+function selectText(id) {
+   document.getElementById("tituloform").select();
+}
 
-        function novaPergunta() {
-            if (id == 1 || opcao == 0 && radio == 0) {
-                alert("Por favor primeiro insira sua pergunta!");
-            } else {
-                var d1 = document.getElementById('drop1');
-                var da = document.getElementById('rd');
-                var db = document.getElementById('opc');
+function novaPergunta() {
+   if (id == 1 || opcao == 0 && radio == 0) {
+      alert("Por favor primeiro insira sua pergunta!");
+   } else {
+      var d1 = document.getElementById('drop1');
+      var da = document.getElementById('rd');
+      var db = document.getElementById('opc');
 
-                //da.setAttribute("disabled", false);
-                //db.setAttribute("disabled", false);
+      d1.insertAdjacentHTML('beforeend',
+         `<textarea placeholder="Digite sua pergunta... " id="${id}" class="novaPergunta"></textarea>`
+      )
 
-                d1.insertAdjacentHTML('beforeend',
-                    `<textarea placeholder="Digite sua pergunta... " id="${id}" class="novaPergunta"></textarea>`
-                )
+      id++;
+      opcao = 0;
+      radio = 0;
+   }
+}
 
-                id++;
-                opcao = 0;
-                radio = 0;
-            }
-        }
+function novaRespostaTexto() {
+   if (id == 0) {
+      alert("Por favor primeiro insira sua pergunta!");
+   } else {
+      var d1 = document.getElementById('drop1');
+      d1.insertAdjacentHTML('beforeend',
+         `<div class="txt" maxlength ="500"><textarea id="${id}" class="novaResposta"></textarea> `);
 
-        function novaRespostaTexto() {
-            if (id == 0) {
-                alert("Por favor primeiro insira sua pergunta!");
-            } else {
-                var d1 = document.getElementById('drop1');
-                d1.insertAdjacentHTML('beforeend',
-                    `<div class="txt" maxlength ="500"><textarea id="${id}" class="novaResposta"></textarea> `);
+      id++;
+      opcao++;
+   } 
+}
 
-                id++;
-                opcao++;
-            }
-            //dc.setAttribute("disabled", true);
-        }
+function novoCheckbox() {
+   if (id == 0) {
+      alert("Por favor primeiro insira sua pergunta!");
+   } else {
+      var d1 = document.getElementById('drop1');
+      var dc = document.getElementById('rd');
 
-        function novoCheckbox() {
-            if (id == 0) {
-                alert("Por favor primeiro insira sua pergunta!");
-            } else {
-                var d1 = document.getElementById('drop1');
-                var dc = document.getElementById('rd');
-
-                d1.insertAdjacentHTML('beforeend',
-                    `<div class="op"><input type="checkbox" id="${id}" ></div> `);
-                id++;
-                d1.insertAdjacentHTML('beforeend',
-                    `<div><input type="text" id="${id}" value=" Opção ${opcao}"></div>
+      d1.insertAdjacentHTML('beforeend',
+         `<div class="op"><input type="checkbox" id="${id}" ></div> `);
+      id++;
+      d1.insertAdjacentHTML('beforeend',
+         `<div><input type="text" id="${id}" value=" Opção ${opcao}"></div>
                      `);
-                id++;
+      id++;
 
-                opcao++;
-            }
-            //dc.setAttribute("disabled", true);
-        }
+      opcao++;
+   }
 
-        function novaRadio() {
+}
 
-            if (id == 0) {
-                alert("Por favor primeiro insira sua pergunta!");
-            } else {
-                var d1 = document.getElementById('drop1');
-                var dd = document.getElementById('opc');
-                d1.insertAdjacentHTML('beforeend',
-                    `<div class="op"> <input type="radio" id="${id}" name="sexo" value="Radio ${radio}"></div>                     
+function novaRadio() {
+
+   if (id == 0) {
+      alert("Por favor primeiro insira sua pergunta!");
+   } else {
+      var d1 = document.getElementById('drop1');
+      var dd = document.getElementById('opc');
+      d1.insertAdjacentHTML('beforeend',
+         `<div class="op"> <input type="radio" id="${id}" name="sexo" value="Radio ${radio}"></div>                     
                      `);
-                id++;
-                d1.insertAdjacentHTML('beforeend',
-                    `<div ><input type="text" id="${id}" value=" Radio ${radio}"></div>
+      id++;
+      d1.insertAdjacentHTML('beforeend',
+         `<div ><input type="text" id="${id}" value=" Radio ${radio}"></div>
                      `);
-                id++;
+      id++;
+      radio++;
+   }
+}
 
-                radio++;
+function deletaUltimoElemento() {
 
+   var el = document.getElementById(id - 1);
+   el.parentNode.removeChild(el);
+   id--;
+   if (radio > 1) {
+      radio--;
+   }
+   if (opcao > 1) {
+      opcao--;
+   }
+   if (radio == 0) {
+      radio = 1;
+   }
+   if (opcao == 0) {
+      opcao = 1;
+   }
+}
 
-                //dd.setAttribute("disabled", true);
-            }
-        }
+function deletaTodosElementos() {
+   var x = id - 1;
 
-        function deletaUltimoElemento() {
+   for (i = 0; i <= x; i++) {
+      var el = document.getElementById(i);
+      el.parentNode.removeChild(el);
+   }
 
-            var el = document.getElementById(id - 1);
-            el.parentNode.removeChild(el);
-            id--;
-            if (radio > 1) {
-                radio--;
-            }
-            if (opcao > 1) {
-                opcao--;
-            }
-            if (radio == 0) {
-                radio = 1;
-            }
-            if (opcao == 0) {
-                opcao = 1;
-            }
-        }
+   radio = 1;
+   opcao = 1;
+   id = 0;
+   carregarTudo();
+}
 
-        function deletaTodosElementos() {
-            var x = id - 1;
+function salvarPagina() {
 
-            for (i = 0; i <= x; i++) {
-                var el = document.getElementById(i);
-                el.parentNode.removeChild(el);
-            }
+   var refVal = document.getElementById("drop1").innerHTML;
+   localStorage.setItem("html", refVal);
+   for (var i = 0; i < id; i++) {
+      var x = document.getElementById(i).value;
+      var y = document.getElementById(i).getAttribute("value");
+      console.log(x);
 
-            radio = 1;
-            opcao = 1;
-            id = 0;
-            carregarTudo();
-        }
+      localStorage.setItem(i, x);
+   }
+}
 
-        function salvarPagina() {
+function atualizaTitulo() {
+   var x = document.getElementById("tituloform").value;
+   localStorage.setItem("x", x);
+}
 
-            var refVal = document.getElementById("drop1").innerHTML;
-            localStorage.setItem("html", refVal);
-            for (var i = 0; i < id; i++) {
-                var x = document.getElementById(i).value;
-                var y = document.getElementById(i).getAttribute("value");
-                console.log(x);
+function atualizaDados() {
+   var x = document.getElementById(id).value;
+   localStorage.setItem(id, x);
+}
 
-                localStorage.setItem(i, x);
-            }
-        }
+function atualizaRadio() {
 
-        function atualizaTitulo() {
-            var x = document.getElementById("tituloform").value;
-            localStorage.setItem("x", x);
-        }
+   var x = document.getElementById(id).value;
+   localStorage.setItem(id, x);
+}
 
-        function atualizaDados() {
-            var x = document.getElementById(id).value;
-            localStorage.setItem(id, x);
-        }
+function atualizaCheck() {
+   var x = document.getElementById("tituloform").value;
+   localStorage.setItem("x", x);
+}
 
-        function atualizaRadio() {
+function retornaDados() {
+   var u = localStorage.length;
+   var valorTemp = localStorage.getItem("html");
+   var x = localStorage.getItem('x');
 
-            var x = document.getElementById(id).value;
-            localStorage.setItem(id, x);
-        }
+   document.getElementById('drop1').innerHTML = valorTemp;
+   document.getElementById('tituloform').value = x;
 
-        function atualizaCheck() {
-            var x = document.getElementById("tituloform").value;
-            localStorage.setItem("x", x);
-        }
+   for (var i = 0; i < u; i++) {      
+      var w = i;
+      var y = localStorage.getItem(w);
+      document.getElementById(w).value = y;
+   }
 
-        function retornaDados() {
-            var u = localStorage.length;
-            var valorTemp = localStorage.getItem("html");
-            var x = localStorage.getItem('x');
-
-            document.getElementById('drop1').innerHTML = valorTemp;
-            document.getElementById('tituloform').value = x;
-
-            for (var i = 0; i < u; i++) {
-                console.log(i);
-                var w = i;
-                var y = localStorage.getItem(w);
-                var z = document.getElementById(w).value = y;
-            }
-
-            localStorage.clear();
-        }
-   
+   localStorage.clear();
+}
